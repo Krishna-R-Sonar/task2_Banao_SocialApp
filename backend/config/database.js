ca773a9c7a2fac01596eaca7d349846458b77004
Krishna-R-Sonar <krishnarsonar@gmail.com>
2025-01-19 16:15:19 +0530

    refactor: update database connection to use environment variable

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true);
 
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGODB_URI || "mongodb+srv://krishnarsonar:krishna9421241525@cluster0.s7a5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .connect(process.env.MONGODB_URI)
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
